92f4d8cafba97a0d83fc5b40c9d0d56e58d0cf5d
sebas2906perre <sebasleon20061988@gmail.com>
2025-05-14 14:50:07 -0500

    tercer commit

---
+++ SQLFILE4.sql
@@ -0,0 +1,12 @@
+USE consultas;
+CALL InsertFabricante('Asus');
+CALL InsertFabricante('Lenovo');
+CALL InsertFabricante('Hewlett-Packard');
+CALL InsertFabricante('Samsung');
+CALL InsertFabricante('Seagate');
+CALL InsertFabricante('Crucial');
+CALL InsertFabricante('Gigabyte');
+CALL InsertFabricante('Huawei');
+CALL InsertFabricante('Xiaomi');
+
+SELECT * FROM fabricante;
